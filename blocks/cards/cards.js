9a2fcddc411d9ac09b41108133169519877acae6
mayursatav <mayursatav9@gmail.com>
2025-02-05 10:25:14 +0530

    Updated card component with react code

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,24 +1,43 @@
+import React, { useEffect } from 'react';
 import { createOptimizedPicture } from '../../scripts/aem.js';
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
-export default function decorate(block) {
-  /* change to ul, li */
-  const ul = document.createElement('ul');
-  [...block.children].forEach((row) => {
-    const li = document.createElement('li');
-    moveInstrumentation(row, li);
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+const Cards = ({ block }) => {
+  useEffect(() => {
+    if (!block) return;
+    
+    const listItems = [...block.children].map((row, index) => {
+      const li = document.createElement('li');
+      moveInstrumentation(row, li);
+      
+      while (row.firstElementChild) li.append(row.firstElementChild);
+      
+      [...li.children].forEach((div) => {
+        if (div.children.length === 1 && div.querySelector('picture')) {
+          div.className = 'cards-card-image';
+        } else {
+          div.className = 'cards-card-body';
+        }
+      });
+      
+      return li;
     });
-    ul.append(li);
-  });
-  ul.querySelectorAll('picture > img').forEach((img) => {
-    const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
-    moveInstrumentation(img, optimizedPic.querySelector('img'));
-    img.closest('picture').replaceWith(optimizedPic);
-  });
-  block.textContent = '';
-  block.append(ul);
-}
+    
+    listItems.forEach((li) => {
+      li.querySelectorAll('picture > img').forEach((img) => {
+        const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
+        moveInstrumentation(img, optimizedPic.querySelector('img'));
+        img.closest('picture').replaceWith(optimizedPic);
+      });
+    });
+    
+    block.textContent = '';
+    const ul = document.createElement('ul');
+    listItems.forEach((li) => ul.append(li));
+    block.append(ul);
+  }, [block]);
+
+  return <div ref={(el) => el && (block = el)} className="cards-container" />;
+};
+
+export default Cards;
